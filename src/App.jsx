c9680c1c79c5c190c613f4a119fa491f5dc77e8a
Harshvardhan Patil <harshvardhanpatil@Harshvardhans-MacBook-Air.local>
2025-04-14 23:34:08 +0100

    optimised

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Garage from "./components/Garage";
 import CarDetails from "./components/CarDetails"; 
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
@@ -12,8 +10,6 @@ function App() {
         <Route path="/" exact component={Garage} />
         <Route path="/car/:registrationNumber" component={CarDetails} />
       </Switch>
-      <Analytics />
-      <SpeedInsights/>
     </Router>
   );
 }
--- vite.config.mjs
+++ vite.config.mjs
@@ -3,31 +3,26 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export default defineConfig(({ command }) => ({
-  server:
-    command === "serve"
-      ? {
-          proxy: {
-            "/api": {
-              target: process.env.VITE_API_URL,
-              changeOrigin: true,
-              rewrite: (path) => path.replace(/^\/api/, ""),
-              configure: (proxy) => {
-                proxy.on("proxyReq", (proxyReq, req, res) => {
-                  console.log(`[Request]\t ${req.method}\t ${req.url}`);
-                  proxyReq.setHeader("x-api-key", process.env.VITE_API_KEY);
-                  proxyReq.setHeader("Content-Type", "application/json");
-                });
-                proxy.on("proxyRes", (proxyRes, req, res) => {
-                  proxyRes.on("end", () => {
-                    console.log(
-                      `[Response]\t ${req.method}\t ${req.url} - Status: ${proxyRes.statusCode}`
-                    );
-                  });
-                });
-              },
-            },
-          },
-        }
-      : undefined,
-}));
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        configure: (proxy) => {
+          proxy.on("proxyReq", (proxyReq, req, res) => {
+            console.log(`[Request]\t ${req.method}\t ${req.url}`);
+            proxyReq.setHeader("x-api-key", process.env.VITE_API_KEY);
+            proxyReq.setHeader("Content-Type", "application/json");
+          });
+          proxy.on("proxyRes", (proxyRes, req, res) => {
+            proxyRes.on("end", () => {
+              console.log(`[Response]\t ${req.method}\t ${req.url} - Status: ${proxyRes.statusCode}`);
+            });
+          });
+        },
+      },
+    },
+  },
+});
