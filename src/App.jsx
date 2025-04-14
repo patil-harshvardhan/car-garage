831a0dcf0fa4fae665794321f092810c7c635983
Harshvardhan Patil <harshvardhanpatil@Harshvardhans-MacBook-Air.local>
2025-04-14 21:49:46 +0100

    Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Garage from "./components/Garage";
 import CarDetails from "./components/CarDetails"; 
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" exact component={Garage} />
         <Route path="/car/:registrationNumber" component={CarDetails} />
       </Switch>
+      <Analytics />
     </Router>
   );
 }
