1764faa980a1b8cfe9ffaf45eecc0842792387c2
Harshvardhan Patil <harshvardhanpatil@Harshvardhans-MacBook-Air.local>
2025-04-14 23:11:59 +0100

    CICD

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,31 +1,33 @@
 import { defineConfig } from "vite";
 import dotenv from "dotenv";
-import preact from "@preact/preset-vite";
 
 dotenv.config();
 
-export default defineConfig({
-  plugins:[preact()],
-  base:"car-garage",
-  server: {
-    proxy: {
-      "/api": {
-        target: process.env.VITE_API_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        configure: (proxy) => {
-          proxy.on("proxyReq", (proxyReq, req, res) => {
-            console.log(`[Request]\t ${req.method}\t ${req.url}`);
-            proxyReq.setHeader("x-api-key", process.env.VITE_API_KEY);
-            proxyReq.setHeader("Content-Type", "application/json");
-          });
-          proxy.on("proxyRes", (proxyRes, req, res) => {
-            proxyRes.on("end", () => {
-              console.log(`[Response]\t ${req.method}\t ${req.url} - Status: ${proxyRes.statusCode}`);
-            });
-          });
-        },
-      },
-    },
-  },
-});
+export default defineConfig(({ command }) => ({
+  server:
+    command === "serve"
+      ? {
+          proxy: {
+            "/api": {
+              target: process.env.VITE_API_URL,
+              changeOrigin: true,
+              rewrite: (path) => path.replace(/^\/api/, ""),
+              configure: (proxy) => {
+                proxy.on("proxyReq", (proxyReq, req, res) => {
+                  console.log(`[Request]\t ${req.method}\t ${req.url}`);
+                  proxyReq.setHeader("x-api-key", process.env.VITE_API_KEY);
+                  proxyReq.setHeader("Content-Type", "application/json");
+                });
+                proxy.on("proxyRes", (proxyRes, req, res) => {
+                  proxyRes.on("end", () => {
+                    console.log(
+                      `[Response]\t ${req.method}\t ${req.url} - Status: ${proxyRes.statusCode}`
+                    );
+                  });
+                });
+              },
+            },
+          },
+        }
+      : undefined,
+}));
