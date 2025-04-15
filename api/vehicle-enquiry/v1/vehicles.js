6d0b0151b958402d56ef804e6b0772dbcba1b049
Harshvardhan Patil <harshvardhanpatil@Harshvardhans-MacBook-Air.local>
2025-04-15 14:18:40 +0100

    error handeling for serverless

--- api/vehicle-enquiry/v1/vehicles.js
+++ api/vehicle-enquiry/v1/vehicles.js
@@ -1,26 +1,40 @@
 export async function POST(req) {
-  const body = await req.json();
-  const { registrationNumber } = body;
+  try {
+    const body = await req.json();
+    const { registrationNumber } = body;
 
-  // Forward the request to the real API
-  console.log("Received registration number:", registrationNumber);
+    const apiRes = await fetch(process.env.VITE_API_URL, {
+      method: "POST",
+      headers: {
+        "x-api-key": process.env.VITE_API_KEY,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ registrationNumber }),
+    });
+    const data = await apiRes.json();
 
-  console.log(process.env.VITE_API_URL);
-  const apiRes = await fetch(process.env.VITE_API_URL, {
-    method: "POST",
-    headers: {
-      "x-api-key": process.env.VITE_API_KEY,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ registrationNumber }),
-  });
-  const data = await apiRes.json();
-  console.log("API response:", data);
-  return new Response(JSON.stringify(data), {
-    status: apiRes.status,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-
+    return new Response(JSON.stringify(data), {
+      status: apiRes.status,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching car details:", error);
+    return new Response(
+      JSON.stringify({
+        errors: [
+          {
+            status: "500",
+            code: "500",
+            title: "Internal Server Error",
+            detail: "Something went wrong while fetching car details.",
+          },
+        ],
+      }),
+      {
+        status: 500,
+      }
+    );
+  }
 }
